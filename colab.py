53709a232b6d440d982bd4f1f5ab50c4a36d01f8
shivanshuman021 <shivanshuman021@gmail.com>
2020-05-07 22:52:46 +0530

    Updated

--- colab.py
+++ colab.py
@@ -26,11 +26,11 @@ def create_model():
 
 
 def load_dataset():
-    train_dataset = h5py.File('train_signs.h5', "r")
+    train_dataset = h5py.File('datasets/train_signs.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('test_signs.h5', "r")
+    test_dataset = h5py.File('datasets/test_signs.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
